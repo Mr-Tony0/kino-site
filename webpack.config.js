aac958299e0ff30c48bcbfbf75032c2b2bb30e18
MrFreyd <antosha.konkov@mail.ru>
2019-11-06 13:42:27 +0700

    webpack options

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack')
+module.exports = {
+ 
+  entry: './src/js/index.js'
+ 
+}
+/*
+plugin: [
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		jQuery: "jquery"
+		})
+	]
+	*/
